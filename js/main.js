25c29d6871c43f1f2d194b87a14ce41603406fbb
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-05-03 14:17:13 +0700

    full page of rules

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-function init() {
+async function init() {
+    const info = await (await fetch('../info.json')).json();
+    document.getElementById('day-number').innerText = info.day.toString();
+    document.getElementById('theme').innerText = info.theme;
 }
