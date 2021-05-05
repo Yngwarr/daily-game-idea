a539925b9f94b4ec99625e7b2444e09f65f2cb0c
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-05-05 16:38:33 +0700

    Typo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 async function init() {
-    const info = await (await fetch('../info.json')).json();
+    const info = await (await fetch('./info.json')).json();
     document.getElementById('day-number').innerText = info.day.toString();
     document.getElementById('theme').innerText = info.theme;
 }
